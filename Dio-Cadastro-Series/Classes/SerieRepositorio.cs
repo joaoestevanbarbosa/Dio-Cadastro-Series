748604aea85c52089c134f2060b24fbebab31df8
João Estevan Barbosa <jestevan12@gmail.com>
2021-12-15 20:41:39 -0300

    iRepositorio , SerieRepositorio

---
+++ Dio-Cadastro-Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Dio_Cadastro_Series.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dio_Cadastro_Series
+{
+    public class SerieRepositorio : iRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dio-Cadastro-Series/Interface/iRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio_Cadastro_Series.Interface
+{
+    public interface iRepositorio<T>
+    {
+        List<T> Lista();
+
+        T RetornaPorId(int id);
+
+        void Insere(T entidade);
+
+        void Atualiza(int id, T entidade);
+
+        int ProximoId();
+        
+    }
+}
