983c1dcdde5cbf037c709dd24e00f399ad4562c8
João Estevan Barbosa <jestevan12@gmail.com>
2021-12-15 20:23:43 -0300

    Adicionar arquivos de projeto.

---
+++ Dio-Cadastro-Series/Classes/Serie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio_Cadastro_Series
+{
+    class Serie : EntidadeBase
+    {
+
+        //Atributos
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        //Métodos
+
+        public Serie (int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            //Enviroment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+
+            string retorno = "";
+
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public string retornaId()
+        {
+            return this.Id.ToString() ;
+        }
+    }
+}
---
+++ Dio-Cadastro-Series/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dio_Cadastro_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
